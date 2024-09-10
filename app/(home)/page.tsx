9758b9c1d163253b43968bbd05f809d3b9af5576
sungwon <thesungwon@gmail.com>
2024-09-10 23:20:56 +0900

    Client Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,32 @@
-export const metadata = {
-  title: "Home",
-  description: "The best movies on the best framework.",
-};
+"use client";
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//   title: "Home",
+//   description: "The best movies on the best framework.",
+// };
 
 const Page = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMovies = async () => {
+    const response = await (
+      await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+    ).json();
+
+    setMovies(response);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>hello</h1>
+      {isLoading ? "isLoading..." : JSON.stringify(movies)}
     </div>
   );
 };
